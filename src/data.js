f2178b2f13e8868ea2fec19a0d3f1c7deefcb3bb
Mabruk <mabruk33@gmail.com>
2024-05-26 16:30:52 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,3 @@
+export let moviesList = ['Gold Coast Lounge', 'Kung Fu Panda 4', 'Avengers', 'Jurassic World', "Sidechic Gang", "Azali", "Master and 3 maids", "The Garfield Movie"];
+export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM", "11:30 PM"];
+export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
